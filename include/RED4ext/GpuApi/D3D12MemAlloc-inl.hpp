9edb8fe1247c7929152b792db2e49ff172dd2fc2
Mozz3d <callmemozzed@gmail.com>
2025-01-07 14:00:49 -0500

    Fix formatting and some mistakes

--- include/RED4ext/GpuApi/D3D12MemAlloc-inl.hpp
+++ include/RED4ext/GpuApi/D3D12MemAlloc-inl.hpp
@@ -4,27 +4,18 @@
 #include <RED4ext/GpuApi/D3D12MemAlloc.hpp>
 #endif
 
-#include <RED4ext/Relocation.hpp>
 #include <RED4ext/Detail/AddressHashes.hpp>
+#include <RED4ext/Relocation.hpp>
 
-RED4EXT_INLINE HRESULT RED4ext::D3D12MA::Allocator::CreateResource(
-        const ALLOCATION_DESC* pAllocDesc,
-        const D3D12_RESOURCE_DESC* pResourceDesc,
-        D3D12_RESOURCE_STATES InitialResourceState,
-        Allocation** ppAllocation,
-        REFIID riidResource,
-        void** ppvResource
-    )
+RED4EXT_INLINE HRESULT RED4ext::D3D12MA::Allocator::CreateResource(const ALLOCATION_DESC* pAllocDesc,
+                                                                   const D3D12_RESOURCE_DESC* pResourceDesc,
+                                                                   D3D12_RESOURCE_STATES InitialResourceState,
+                                                                   Allocation** ppAllocation,
+                                                                   REFIID riidResource,
+                                                                   void** ppvResource)
 {
-    using func_t = HRESULT (*)(
-        Allocator*,
-        const ALLOCATION_DESC*, 
-        const D3D12_RESOURCE_DESC*,
-        D3D12_RESOURCE_STATES,
-        Allocation**,
-        REFIID,
-        void**
-    );
+    using func_t = HRESULT (*)(Allocator*, const ALLOCATION_DESC*, const D3D12_RESOURCE_DESC*, D3D12_RESOURCE_STATES,
+                               Allocation**, REFIID, void**);
     static UniversalRelocFunc<func_t> func(Detail::AddressHashes::Allocator_CreateResource);
     return func(this, pAllocDesc, pResourceDesc, InitialResourceState, ppAllocation, riidResource, ppvResource);
 }
--- include/RED4ext/GpuApi/D3D12MemAlloc.hpp
+++ include/RED4ext/GpuApi/D3D12MemAlloc.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <RED4ext/Common.hpp>
 #include <cstdint>
 #include <d3d12.h>
-#include <RED4ext/Common.hpp>
 
-// this file is a recreation of D3D12MemAlloc.h from https://github.com/GPUOpen-LibrariesAndSDKs/D3D12MemoryAllocator as it exists in Cyberpunk 2077
+// this file is a recreation of D3D12MemAlloc.h from https://github.com/GPUOpen-LibrariesAndSDKs/D3D12MemoryAllocator as 
+// it exists in Cyberpunk 2077
 
 namespace RED4ext
 {
@@ -15,7 +16,8 @@ struct IUnknownImpl
 {
     void* vftable;
     uint32_t m_RefCount;
-}
+};
+RED4EXT_ASSERT_SIZE(IUnknownImpl, 0x10);
 
 struct Allocation : IUnknownImpl
 {
@@ -26,7 +28,7 @@ struct Allocation : IUnknownImpl
     void* m_pPrivateData;       // 30
     wchar_t* m_Name;            // 38
     uint8_t unk40[0x28];        // 40
-}
+};
 RED4EXT_ASSERT_SIZE(Allocation, 0x68);
 
 enum ALLOCATION_FLAGS 
@@ -43,9 +45,7 @@ enum ALLOCATION_FLAGS
     ALLOCATION_FLAG_STRATEGY_BEST_FIT = ALLOCATION_FLAG_STRATEGY_MIN_MEMORY,
     ALLOCATION_FLAG_STRATEGY_FIRST_FIT = ALLOCATION_FLAG_STRATEGY_MIN_TIME,
     ALLOCATION_FLAG_STRATEGY_MASK =
-        ALLOCATION_FLAG_STRATEGY_MIN_MEMORY |
-        ALLOCATION_FLAG_STRATEGY_MIN_TIME |
-        ALLOCATION_FLAG_STRATEGY_MIN_OFFSET
+        ALLOCATION_FLAG_STRATEGY_MIN_MEMORY | ALLOCATION_FLAG_STRATEGY_MIN_TIME | ALLOCATION_FLAG_STRATEGY_MIN_OFFSET
 };
 
 struct ALLOCATION_DESC
@@ -55,18 +55,13 @@ struct ALLOCATION_DESC
     D3D12_HEAP_FLAGS ExtraHeapFlags;
     void* CustomPool;
     void* pPrivateData;
-}
+};
 
 struct Allocator : IUnknownImpl
 {
-    HRESULT CreateResource(
-        const ALLOCATION_DESC* pAllocDesc,
-        const D3D12_RESOURCE_DESC* pResourceDesc,
-        D3D12_RESOURCE_STATES InitialResourceState,
-        Allocation** ppAllocation,
-        REFIID riidResource,
-        void** ppvResource
-    );
+    HRESULT CreateResource(const ALLOCATION_DESC* pAllocDesc, const D3D12_RESOURCE_DESC* pResourceDesc,
+                           D3D12_RESOURCE_STATES InitialResourceState, Allocation** ppAllocation, REFIID riidResource,
+                           void** ppvResource);
 
     uint8_t unk10[0x18 - 0x10]; // 10
 };
