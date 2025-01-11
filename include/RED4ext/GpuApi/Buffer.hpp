395128b73c664d800a9c4dc2f988a1231b1449ab
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2025-01-11 11:21:33 -0500

    Update include/RED4ext/GpuApi/Buffer.hpp
    Co-authored-by: Octavian Dima <3403191+wopss@users.noreply.github.com>

--- include/RED4ext/GpuApi/Buffer.hpp
+++ include/RED4ext/GpuApi/Buffer.hpp
@@ -13,7 +13,7 @@ struct SBufferData
     uint8_t unk04[0x10 - 0x04];                            // 04
     Microsoft::WRL::ComPtr<ID3D12Resource> bufferResource; // 10
     void* unk18;                                           // 18
-    uint8_t unk20[0xa8 - 0x20];                            // 0x20
+    uint8_t unk20[0xa8 - 0x20];                            // 20
 };
 RED4EXT_ASSERT_SIZE(SBufferData, 0xa8);
 RED4EXT_ASSERT_OFFSET(SBufferData, bufferResource, 0xa8);
