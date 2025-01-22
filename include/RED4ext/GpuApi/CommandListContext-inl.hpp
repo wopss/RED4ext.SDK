95ad67f260c14b67f467ed8ede5474c2c04009ad
psiberx <psiberx@gmail.com>
2025-01-22 23:19:14 +0300

    Use UniquePtr for CommandListContext

--- include/RED4ext/GpuApi/CommandListContext-inl.hpp
+++ include/RED4ext/GpuApi/CommandListContext-inl.hpp
@@ -8,6 +8,13 @@
 
 namespace RED4ext::GpuApi
 {
+RED4EXT_INLINE CommandListContext::~CommandListContext()
+{
+    using func_t = void (*)(CommandListContext*);
+    static const UniversalRelocFunc<func_t> func(Detail::AddressHashes::CommandListContext_dtor);
+    func(this);
+}
+
 RED4EXT_INLINE void CommandListContext::AddPendingBarrier(const D3D12_RESOURCE_BARRIER& aBarrier)
 {
     using func_t = void (*)(CommandListContext*, const D3D12_RESOURCE_BARRIER&);
@@ -29,16 +36,17 @@ RED4EXT_INLINE void CommandListContext::FlushPendingBarriers()
     func(this);
 }
 
-RED4EXT_INLINE CommandListContext* AcquireFreeCommandList(CommandListType aType, const StringView& aDebugName,
-                                                          uint64_t aHash)
+RED4EXT_INLINE UniquePtr<CommandListContext> AcquireFreeCommandList(CommandListType aType, const StringView& aDebugName,
+                                                                    uint64_t aHash)
 {
     // NOTE: This function has parameters for debug name and hash which seem to be optional.
-    // Expects unique ptr as an out param and returns it by reference.
-    using func_t = CommandListContext*& (*)(CommandListContext*&, CommandListType, const StringView&, uint64_t);
+    using func_t = UniquePtr<CommandListContext>* (*)(UniquePtr<CommandListContext>&, CommandListType,
+                                                      const StringView&, uint64_t);
     static const UniversalRelocFunc<func_t> func(Detail::AddressHashes::GetFreeCommandList);
 
-    // TODO: This should be unique_ptr which function fills in and returns.
-    CommandListContext* outContext = nullptr;
-    return func(outContext, aType, aDebugName, aHash);
+    UniquePtr<CommandListContext> outContext;
+    func(outContext, aType, aDebugName, aHash);
+
+    return std::move(outContext);
 }
 } // namespace RED4ext::GpuApi
