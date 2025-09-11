5e5d4fba9917a58a9622c72c4c902dfd27f553b8
Octavian Dima <3403191+wopss@users.noreply.github.com>
2025-09-11 19:07:15 +0300

    Add support for patch 2.31

--- src/Scripting/Natives.cpp
+++ src/Scripting/Natives.cpp
@@ -2867,6 +2867,10 @@
 #include <RED4ext/Scripting/Natives/Generated/vehicle/CameraManager.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/CameraPerspective.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/CarBaseObject.hpp>
+#include <RED4ext/Scripting/Natives/Generated/vehicle/CinematicCameraShot.hpp>
+#include <RED4ext/Scripting/Natives/Generated/vehicle/CinematicCameraShotEffect.hpp>
+#include <RED4ext/Scripting/Natives/Generated/vehicle/CinematicCameraShotGroup.hpp>
+#include <RED4ext/Scripting/Natives/Generated/vehicle/CinematicCameraShotStopCondition.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/DestructionPSData.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/DisabledReason.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/ELightMode.hpp>
@@ -2887,6 +2891,7 @@
 #include <RED4ext/Scripting/Natives/Generated/vehicle/PlayerToAIInterpolationType.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/PoliceStrategy.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/SummonState.hpp>
+#include <RED4ext/Scripting/Natives/Generated/vehicle/TimedCinematicCameraShotEffect.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/UnlockedVehicle.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/VehicleAppearanceToDecalsName.hpp>
 #include <RED4ext/Scripting/Natives/Generated/vehicle/VehicleClearCoatOverrides.hpp>
