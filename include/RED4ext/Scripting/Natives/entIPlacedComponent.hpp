0072faac2530caa09adb240b44b351356d1e46f4
Louis Poirier <contact@louispoirier.io>
2025-11-21 16:09:55 +0100

    feat: add decoded members for IPlacedComponent
    - types and offsets of lastPosition/position is sure but names is a best guess effort
    - ChildBinding might contain Handle and/or WeakHandle, needs runtime check
    - DynArray<ChildBinding> fits the memory but this might actually be a fixed-size array, needs runtime check

--- include/RED4ext/Scripting/Natives/entIPlacedComponent.hpp
+++ include/RED4ext/Scripting/Natives/entIPlacedComponent.hpp
@@ -19,14 +19,31 @@ struct __declspec(align(0x10)) IPlacedComponent : ent::IComponent
 {
     static constexpr const char* NAME = "entIPlacedComponent";
     static constexpr const char* ALIAS = "IPlacedComponent";
+    
+    struct ChildBinding
+    {
+        Handle<ent::IComponent> component;   // 00
+        Handle<ent::IAttachment> attachment; // 10
+    };
 
     Handle<ent::ITransformBinding> parentTransform; // 90
-    uint8_t unkA0[0xC0 - 0xA0];                     // A0
+    DynArray<ChildBinding> childrenTransforms;      // A0
+    uint8_t unkB0[0xC0 - 0xB0];                     // B0
     WorldTransform localTransform;                  // C0
     WorldTransform worldTransform;                  // E0
-    uint8_t unkE0[0x120 - 0x100];                   // 100
+    Vector4 lastPosition;                           // 100
+    Vector4 position;                               // 110
 };
+RED4EXT_ASSERT_SIZE(IPlacedComponent::ChildBinding, 0x20);
+RED4EXT_ASSERT_OFFSET(IPlacedComponent::ChildBinding, component, 0x00);
+RED4EXT_ASSERT_OFFSET(IPlacedComponent::ChildBinding, attachment, 0x10);
+
 RED4EXT_ASSERT_SIZE(IPlacedComponent, 0x120);
+RED4EXT_ASSERT_OFFSET(IPlacedComponent, parentTransform, 0x90);
+RED4EXT_ASSERT_OFFSET(IPlacedComponent, localTransform, 0xC0);
+RED4EXT_ASSERT_OFFSET(IPlacedComponent, worldTransform, 0xE0);
+RED4EXT_ASSERT_OFFSET(IPlacedComponent, lastPosition, 0x100);
+RED4EXT_ASSERT_OFFSET(IPlacedComponent, position, 0x110);
 } // namespace ent
 using entIPlacedComponent = ent::IPlacedComponent;
 using IPlacedComponent = ent::IPlacedComponent;
