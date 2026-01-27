204c0e771f79e91ce955440f1c504424a3019501
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2026-01-26 20:50:46 -0500

    Impl `IRenderData` & `TRenderPtr` + organize render types

---
+++ include/RED4ext/Rendering/RenderData.hpp
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <RED4ext/Detail/Memory.hpp>
+
+namespace RED4ext
+{
+struct IRenderData
+{
+    using AllocatorType = Memory::RenderDataAllocator;
+
+    virtual Memory::IAllocator* GetAllocator()
+    {
+        return AllocatorType::Get();
+    }
+
+    virtual void Destroy()
+    {
+        if (this)
+        {
+            Memory::Delete(this);
+        }
+    }
+
+    virtual ~IRenderData() = default;
+
+    void Release()
+    {
+        if (--refCount < 1)
+            Destroy();
+    }
+
+    void AddRef()
+    {
+        refCount++;
+    }
+
+    std::atomic<int32_t> refCount = 1;
+};
+RED4EXT_ASSERT_SIZE(IRenderData, 0x10);
+
+template<std::derived_from<IRenderData> T = IRenderData>
+class TRenderPtr
+{
+    TRenderPtr() = default;
+
+    ~TRenderPtr() noexcept
+    {
+        Release();
+    }
+
+    TRenderPtr(std::nullptr_t) noexcept
+    {
+    }
+
+    template<std::derived_from<IRenderData> U>
+    explicit TRenderPtr(U* aPointer) noexcept
+        : m_instance(aPointer)
+    {
+    }
+
+    TRenderPtr(const TRenderPtr& aOther) noexcept
+        : m_instance(aOther.m_instance)
+    {
+        if (m_instance)
+            m_instance->AddRef();
+    }
+
+    TRenderPtr(TRenderPtr&& aOther) noexcept
+    {
+        Swap(aOther);
+    }
+
+    operator bool() const noexcept
+    {
+        return m_instance != nullptr;
+    }
+
+    TRenderPtr& operator=(const TRenderPtr& aRhs) noexcept
+    {
+        TRenderPtr(aRhs).Swap(*this);
+        return *this;
+    }
+
+    TRenderPtr& operator=(TRenderPtr&& aRhs) noexcept
+    {
+        Swap(aRhs);
+        return *this;
+    }
+
+    T* operator->() const noexcept
+    {
+        return m_instance;
+    }
+
+    void Swap(TRenderPtr& aOther) noexcept
+    {
+        std::swap(m_instance, aOther.m_instance);
+    }
+
+    T* GetPtr() const noexcept
+    {
+        return m_instance;
+    }
+
+private:
+    void Release() noexcept
+    {
+        if (m_instance)
+            m_instance->Release();
+    }
+
+    T* m_instance = nullptr;
+};
+RED4EXT_ASSERT_SIZE(TRenderPtr<>, 0x08);
+} // namespace RED4ext
--- include/RED4ext/Scripting/Natives/entMorphTargetSkinnedMeshComponent.hpp
+++ include/RED4ext/Scripting/Natives/entMorphTargetSkinnedMeshComponent.hpp
@@ -3,7 +3,7 @@
 #include <RED4ext/CName.hpp>
 #include <RED4ext/Common.hpp>
 #include <RED4ext/NativeTypes.hpp>
-#include <RED4ext/RenderProxy.hpp>
+#include <RED4ext/Rendering/RenderProxy.hpp>
 #include <RED4ext/Scripting/Natives/Generated/ent/ISkinTargetComponent.hpp>
 #include <RED4ext/Scripting/Natives/Generated/red/TagList.hpp>
 #include <RED4ext/Scripting/Natives/Generated/shadows/ShadowCastingMode.hpp>
