610f41ec134efc9e115ab3e8fb2d67332c733228
Mozz3d <163353426+Mozz3d@users.noreply.github.com>
2026-03-05 14:28:36 -0500

    Move constexpr methods in `StringView` inline to resolve static lib linker errors

--- include/RED4ext/StringView-inl.hpp
+++ include/RED4ext/StringView-inl.hpp
@@ -6,68 +6,10 @@
 
 #include <RED4ext/String.hpp>
 
-constexpr RED4ext::StringView::StringView() noexcept
-    : m_ptr(nullptr)
-    , m_size(0u)
-{
-}
-
-constexpr RED4ext::StringView::StringView(ConstPointer aStr) noexcept
-    : m_ptr(aStr)
-    , m_size(static_cast<SizeType>(TraitsType::length(aStr)))
-{
-}
-
-constexpr RED4ext::StringView::StringView(std::string_view aView) noexcept
-    : m_ptr(aView.data())
-    , m_size(static_cast<SizeType>(aView.size()))
-{
-}
-
 RED4EXT_INLINE RED4ext::StringView::StringView(const String& aStr) noexcept
     : m_ptr(aStr.Data())
     , m_size(aStr.Size())
 {
-}
-
-constexpr bool RED4ext::StringView::IsEmpty() const noexcept
-{
-    return m_size == 0u;
-}
-
-constexpr RED4ext::StringView::operator bool() const noexcept
-{
-    return !IsEmpty();
-}
-
-constexpr bool RED4ext::StringView::operator==(StringView aRhs) const noexcept
-{
-    return Compare(aRhs);
-}
-
-constexpr bool RED4ext::StringView::operator!=(StringView aRhs) const noexcept
-{
-    return !Compare(aRhs);
-}
-
-constexpr bool RED4ext::StringView::operator==(ConstPointer aRhs) const noexcept
-{
-    return Compare(aRhs);
-}
-
-constexpr bool RED4ext::StringView::operator!=(ConstPointer aRhs) const noexcept
-{
-    return !Compare(aRhs);
-}
-
-constexpr bool RED4ext::StringView::operator==(std::string_view aRhs) const noexcept
-{
-    return Compare(aRhs);
-}
-
-constexpr bool RED4ext::StringView::operator!=(std::string_view aRhs) const noexcept
-{
-    return !Compare(aRhs);
 }
 
 RED4EXT_INLINE bool RED4ext::StringView::operator==(const String& aRhs) const noexcept
@@ -80,73 +22,3 @@ RED4EXT_INLINE bool RED4ext::StringView::operator!=(const String& aRhs) const no
     return !Compare(aRhs);
 }
 
-constexpr RED4ext::StringView::ConstReference RED4ext::StringView::operator[](SizeType aIndex) const noexcept
-{
-    return m_ptr[aIndex];
-}
-
-constexpr RED4ext::StringView::ConstReference RED4ext::StringView::At(SizeType aIndex) const
-{
-    if (aIndex >= m_size)
-        throw std::out_of_range("StringView::At: Position out of range");
-
-    return m_ptr[aIndex];
-}
-
-constexpr bool RED4ext::StringView::Compare(StringView aOther) const noexcept
-{
-    if (IsEmpty() && aOther.IsEmpty())
-        return true;
-
-    if (m_size != aOther.m_size)
-        return false;
-
-    return TraitsType::compare(m_ptr, aOther.m_ptr, m_size) == 0;
-}
-
-constexpr RED4ext::StringView::ConstReference RED4ext::StringView::Front() const noexcept
-{
-    assert(!IsEmpty());
-    return m_ptr[0];
-}
-
-constexpr RED4ext::StringView::ConstReference RED4ext::StringView::Back() const noexcept
-{
-    assert(!IsEmpty());
-    return m_ptr[m_size - 1];
-}
-
-constexpr RED4ext::StringView::ConstIterator RED4ext::StringView::Begin() const noexcept
-{
-    return m_ptr;
-}
-
-constexpr RED4ext::StringView::ConstIterator RED4ext::StringView::End() const noexcept
-{
-    return m_ptr + m_size;
-}
-
-constexpr RED4ext::StringView::ConstReverseIterator RED4ext::StringView::RBegin() const noexcept
-{
-    return ConstReverseIterator(Begin());
-}
-
-constexpr RED4ext::StringView::ConstReverseIterator RED4ext::StringView::REnd() const noexcept
-{
-    return ConstReverseIterator(End());
-}
-
-constexpr RED4ext::StringView::ConstPointer RED4ext::StringView::Data() const noexcept
-{
-    return m_ptr;
-}
-
-constexpr RED4ext::StringView::SizeType RED4ext::StringView::Size() const noexcept
-{
-    return m_size;
-}
-
-constexpr RED4ext::StringView::SizeType RED4ext::StringView::Length() const noexcept
-{
-    return m_size;
-}
--- include/RED4ext/StringView.hpp
+++ include/RED4ext/StringView.hpp
@@ -2,6 +2,8 @@
 
 #include <RED4ext/Common.hpp>
 
+#include <cassert>
+#include <stdexcept>
 #include <string_view>
 
 namespace RED4ext
@@ -28,41 +30,134 @@ public:
 
     using TraitsType = std::char_traits<ValueType>;
 
-    constexpr StringView() noexcept;
-    constexpr StringView(ConstPointer aStr) noexcept;
-    constexpr StringView(std::string_view aView) noexcept;
+    constexpr StringView() noexcept = default;
+
+    constexpr StringView(ConstPointer aStr) noexcept
+        : m_ptr(aStr)
+        , m_size(static_cast<SizeType>(TraitsType::length(aStr)))
+    {
+    }
+
+    constexpr StringView(std::string_view aView) noexcept
+        : m_ptr(aView.data())
+        , m_size(static_cast<SizeType>(aView.size()))
+    {
+    }
+
     StringView(const String& aStr) noexcept;
-
-    constexpr operator bool() const noexcept;
-
-    constexpr bool operator==(StringView aRhs) const noexcept;
-    constexpr bool operator!=(StringView aRhs) const noexcept;
-    constexpr bool operator==(ConstPointer aRhs) const noexcept;
-    constexpr bool operator!=(ConstPointer aRhs) const noexcept;
-    constexpr bool operator==(std::string_view aRhs) const noexcept;
-    constexpr bool operator!=(std::string_view aRhs) const noexcept;
     bool operator==(const String& aRhs) const noexcept;
     bool operator!=(const String& aRhs) const noexcept;
 
-    constexpr ConstReference operator[](SizeType aIndex) const noexcept;
+    constexpr bool operator==(StringView aRhs) const noexcept
+    {
+        return Compare(aRhs);
+    }
 
-    [[nodiscard]] constexpr ConstReference At(SizeType aIndex) const;
+    constexpr bool operator!=(StringView aRhs) const noexcept
+    {
+        return !Compare(aRhs);
+    }
 
-    [[nodiscard]] constexpr bool Compare(StringView aOther) const noexcept;
+    constexpr bool operator==(ConstPointer aRhs) const noexcept
+    {
+        return Compare(aRhs);
+    }
 
-    [[nodiscard]] constexpr ConstReference Front() const noexcept;
-    [[nodiscard]] constexpr ConstReference Back() const noexcept;
+    constexpr bool operator!=(ConstPointer aRhs) const noexcept
+    {
+        return !Compare(aRhs);
+    }
 
-    [[nodiscard]] constexpr ConstIterator Begin() const noexcept;
-    [[nodiscard]] constexpr ConstIterator End() const noexcept;
-    [[nodiscard]] constexpr ConstReverseIterator RBegin() const noexcept;
-    [[nodiscard]] constexpr ConstReverseIterator REnd() const noexcept;
+    constexpr bool operator==(std::string_view aRhs) const noexcept
+    {
+        return Compare(aRhs);
+    }
 
-    [[nodiscard]] constexpr bool IsEmpty() const noexcept;
+    constexpr bool operator!=(std::string_view aRhs) const noexcept
+    {
+        return !Compare(aRhs);
+    }
 
-    [[nodiscard]] constexpr ConstPointer Data() const noexcept;
-    [[nodiscard]] constexpr SizeType Size() const noexcept;
-    [[nodiscard]] constexpr SizeType Length() const noexcept;
+    constexpr operator bool() const noexcept
+    {
+        return !IsEmpty();
+    }
+
+    constexpr ConstReference operator[](SizeType aIndex) const noexcept
+    {
+        return m_ptr[aIndex];
+    }
+
+    [[nodiscard]] constexpr bool Compare(StringView aOther) const noexcept
+    {
+        if (IsEmpty() && aOther.IsEmpty())
+            return true;
+
+        if (m_size != aOther.m_size)
+            return false;
+
+        return TraitsType::compare(m_ptr, aOther.m_ptr, m_size) == 0;
+    }
+
+    [[nodiscard]] constexpr ConstReference At(SizeType aIndex) const
+    {
+        if (aIndex >= m_size)
+        throw std::out_of_range("StringView::At: Position out of range");
+
+        return m_ptr[aIndex];
+    }
+
+    [[nodiscard]] constexpr ConstReference Front() const noexcept
+    {
+        assert(!IsEmpty());
+        return m_ptr[0];
+    }
+
+    [[nodiscard]] constexpr ConstReference Back() const noexcept
+    {
+        assert(!IsEmpty());
+        return m_ptr[m_size - 1];
+    }
+
+    [[nodiscard]] constexpr ConstIterator Begin() const noexcept
+    {
+        return m_ptr;
+    }
+
+    [[nodiscard]] constexpr ConstIterator End() const noexcept
+    {
+        return m_ptr + m_size;
+    }
+
+    [[nodiscard]] constexpr ConstReverseIterator RBegin() const noexcept
+    {
+        return ConstReverseIterator(Begin());
+    }
+
+    [[nodiscard]] constexpr ConstReverseIterator REnd() const noexcept
+    {
+        return ConstReverseIterator(End());
+    }
+
+    [[nodiscard]] constexpr bool IsEmpty() const noexcept
+    {
+        return m_size == 0u;
+    }
+
+    [[nodiscard]] constexpr ConstPointer Data() const noexcept
+    {
+        return m_ptr;
+    }
+
+    [[nodiscard]] constexpr SizeType Size() const noexcept
+    {
+        return m_size;
+    }
+
+    [[nodiscard]] constexpr SizeType Length() const noexcept
+    {
+        return m_size;
+    }
 
 #pragma region STL
     using value_type = ValueType;
@@ -96,8 +191,8 @@ public:
     }
 #pragma endregion
 private:
-    const char* m_ptr;
-    std::uint32_t m_size;
+    const char* m_ptr {nullptr};
+    std::uint32_t m_size {0};
 };
 #pragma pack(pop)
 
