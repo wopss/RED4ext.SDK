be4170d78bb0d5d53cc944720510d8e8d34b9d40
Pavel Siberx <psiberx@gmail.com>
2026-04-25 18:17:05 +0300

    Use UniquePtr for EntityStub (#213)

--- include/RED4ext/Scripting/Natives/IUpdatableSystem-inl.hpp
+++ include/RED4ext/Scripting/Natives/IUpdatableSystem-inl.hpp
@@ -4,8 +4,6 @@
 #include <RED4ext/Scripting/Natives/IUpdatableSystem.hpp>
 #endif
 
-#include <RED4ext/SystemUpdate.hpp>
-
 RED4EXT_INLINE void RED4ext::IUpdatableSystem::OnRegisterUpdates(RED4ext::UpdateRegistrar* aRegistrar)
 {
     RED4EXT_UNUSED_PARAMETER(aRegistrar);
