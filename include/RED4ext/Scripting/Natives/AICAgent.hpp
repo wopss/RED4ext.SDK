5ec38fdd167ffa79d0116b30f12fe848eb8c9b87
Louis Poirier <contact@louispoirier.io>
2026-06-25 23:53:36 +0200

    feat(AICAgent): generate stub, add `behavior::Instance` member

--- include/RED4ext/Scripting/Natives/AICAgent.hpp
+++ include/RED4ext/Scripting/Natives/AICAgent.hpp
@@ -1,22 +1,9 @@
 #pragma once
 
-// clang-format off
-
-// This file is generated from the Game's Reflection data
-
-#include <RED4ext/Scripting/Natives/AICAgent.hpp>
-
-namespace RED4ext
-{
-RED4EXT_ASSERT_SIZE(AI::CAgent, 0x4F0);
-using AICAgent = AI::CAgent;
-using AIComponent = AI::CAgent;
-} // namespace RED4ext
-
-/*
-#include <cstdint>
 #include <RED4ext/Common.hpp>
+#include <RED4ext/Scripting/Natives/Generated/AI/behavior/Instance.hpp>
 #include <RED4ext/Scripting/Natives/Generated/game/Component.hpp>
+#include <cstdint>
 
 namespace RED4ext
 {
@@ -27,13 +14,13 @@ struct CAgent : game::Component
     static constexpr const char* NAME = "AICAgent";
     static constexpr const char* ALIAS = "AIComponent";
 
-    uint8_t unkA8[0x4F0 - 0xA8]; // A8
+    uint8_t unkA8[0xE0 - 0xA8];    // A8
+    behavior::Instance instance;   // E0
+    uint8_t unk1C8[0x4F0 - 0x1C8]; // 1C8
 };
 RED4EXT_ASSERT_SIZE(CAgent, 0x4F0);
+RED4EXT_ASSERT_OFFSET(CAgent, instance, 0xE0);
 } // namespace AI
 using AICAgent = AI::CAgent;
 using AIComponent = AI::CAgent;
 } // namespace RED4ext
-*/
-
-// clang-format on
